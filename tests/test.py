b555a80460f7d63cb8a8012b6f275f97e3bce5ba
Emmet Tracey <emtracey@tcd.ie>
2018-02-27 21:05:44 +0000

    created the basis of the test file

---
+++ tests/test.py
@@ -0,0 +1,19 @@
+# Testing for ledcheck.main
+
+def test_file_existence():
+    pass
+
+def test_file_format():
+    pass
+
+def test_boundaries():
+    pass
+
+def test_turn_on():
+    pass
+
+def test_turn_off():
+    pass
+
+def test_toggle():
+    pass
