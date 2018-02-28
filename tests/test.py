a137db436f5e00154d72a1d5eb88b8b3ae9abc0a
Emmet Tracey <emtracey@tcd.ie>
2018-02-28 13:46:04 +0000

    wrote the first test for file existence

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
 # Testing for ledcheck.main
 
+from ledcheck.main import *
+
+
 def test_file_existence():
-    pass
+    ''' Reads the first 35 characters from the file and checks that this matches the characters I have manually added'''
+    test_file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+    assert file_existence("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")[:35] == "1000\nturn off 660,55 through 986,197"
 
 def test_file_format():
     pass
