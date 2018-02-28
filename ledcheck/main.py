4dc459d1ea95af93c3c6b9fa3d26368820007998
Emmet Tracey <emtracey@tcd.ie>
2018-02-28 13:32:43 +0000

    Added the bones of the main.py file

---
+++ ledcheck/main.py
@@ -0,0 +1,41 @@
+'''
+Created on 27 Feb 2018
+
+@author: Emmet
+'''
+
+''' Create a class for the LightTester'''
+class LightTester():
+    lights = None
+    
+    def __init__(self):
+        pass
+    
+    def file_existence(self):
+        pass
+    
+    def file_format(self):
+        pass
+    
+    def boundaries(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+    
+    def toggle(self):
+        pass
+
+def read_file():
+    pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
