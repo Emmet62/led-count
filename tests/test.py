ff9e13d741473f566c72e049e9a58fb5010806b7
Emmet Tracey <emtracey@tcd.ie>
2018-02-28 19:10:48 +0000

    formatted test.py

--- tests/test.py
+++ tests/test.py
@@ -17,8 +17,8 @@ def test_boundaries():
     ''' Need to write a test that will check that the coordinates are within the boundaries of the grid
     If they aren't, they should be changed'''
     test_grid = LightTester(1000)
-    test_coordinates = test_grid.boundaries([0,1099],[-2,230])
-    assert test_coordinates == ([0,999],[0, 230])
+    test_coordinates = test_grid.boundaries([0, 1099], [-2, 230])
+    assert test_coordinates == ([0, 999],[0, 230])
 
 def test_turn_on():
     pass
