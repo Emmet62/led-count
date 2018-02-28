ed552cb4045d17b06fd85f16487ca7542aa55a1a
Emmet Tracey <emtracey@tcd.ie>
2018-02-28 14:54:22 +0000

    wrote code for check file existence and test works

--- ledcheck/main.py
+++ ledcheck/main.py
@@ -4,16 +4,16 @@ Created on 27 Feb 2018
 @author: Emmet
 '''
 
+import sys
+import requests
+
 ''' Create a class for the LightTester'''
 class LightTester():
     lights = None
     
     def __init__(self):
         pass
-    
-    def file_existence(self):
-        pass
-    
+        
     def file_format(self):
         pass
     
@@ -29,10 +29,16 @@ class LightTester():
     def toggle(self):
         pass
 
+def file_existence(file):
+    ''' Checks that the input file exists (local file or network address)'''
+    if file.startswith('http://'):
+        response = requests.get(file)
+    else:
+        response = open(file, 'r')
+    return response
+
 def read_file():
     pass
-
-
 
 def main():
     pass
--- tests/test.py
+++ tests/test.py
@@ -4,12 +4,13 @@ from ledcheck.main import *
 
 
 def test_file_existence():
-    ''' Reads the first 35 characters from the file and checks that this matches the characters I have manually added'''
     test_file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
-    assert file_existence("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")[:35] == "1000\nturn off 660,55 through 986,197"
+    assert file_existence(test_file) != None
 
 def test_file_format():
-    pass
+    ''' Reads the first 35 characters from the file and checks that this matches the characters I have manually added'''
+    test_file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+    assert file_format("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")[:4] == "1000" #\nturn off 660,55 through 986,197"
 
 def test_boundaries():
     pass
