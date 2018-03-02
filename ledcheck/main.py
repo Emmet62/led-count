77474eb68fbbff0504e8e37ea670ad190ffdfa9c
Emmet Tracey <emtracey@tcd.ie>
2018-03-02 09:49:41 +0000

    added more explanatory comments to main.py

--- ledcheck/main.py
+++ ledcheck/main.py
@@ -41,7 +41,7 @@ class LightTester():
     
     def turn_off(self, start, stop):
         ''' Method that runs from start to stop points, switching lights off
-        On is represented by the number 0'''
+        Off is represented by the number 0'''
         if start[0] <= stop[0] and start[1] <= stop[1]:
             for i in range (start[0], (stop[0] + 1)):
                 for j in range (start[1], (stop[1] + 1)):
@@ -58,6 +58,7 @@ class LightTester():
                         self.grid[i][j] = 0                
                         
     def count(self, gridSize):
+        ''' Method that iterates through the multidimensional array and counts the number of lights on'''
         lights_on = 0
         for i in range (0, gridSize):
             for j in range (0, gridSize):
@@ -75,36 +76,44 @@ def file_existence(file):
     return response 
 
 def get_coord(string):
+    ''' Reads a string and returns coordinates as integers in a list'''
     First = int(string[0])
     Last = int(string[1])
     coord = [First, Last]
     return coord
 
 def main():
+    ''' Checks that command is in required format'''
     if len(sys.argv) < 3:
-        print('Error: No input file provided')
+        print('Error: Please check the parameters\nCommand should be of form: "ledcheck --input file_link"')
     else:
         link = sys.argv[2]
         file = file_existence(link)
-        gridSize = int(file.split("\n")[0])
-        cleanFile = re.findall(r".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*", file)
-        commandNumber = len(cleanFile)
+        gridSize = int(file.split("\n")[0]) # get the grid size from the first line of file
+        cleanFile = re.findall(r".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*", file) 
+        commandNumber = len(cleanFile) # get the number of commands
         print("Grid Size:", gridSize)
         print("No. of Commands:", commandNumber)
-        grid = LightTester(gridSize)
+        grid = LightTester(gridSize) # create an instance of class LightTester
         
+        ''' for loop that iterates through each element in the list
+        The RegEx used above returns the input file as a list of lists
+        Each inner list is in the form ('turn off', '660', '55', '986', '197'), etc.
+        We can index into each inner list and then index into the specific elements in it'''
         for i in range (0, commandNumber):
-            start = get_coord(cleanFile[i][1:3])
-            stop = get_coord(cleanFile[i][3:])
-            boundedStart, boundedStop = grid.boundaries(start, stop)
+            start = get_coord(cleanFile[i][1:3]) # start coordinates are at index 1 and 2
+            stop = get_coord(cleanFile[i][3:]) # stop coordinates are at index 3 and 4
+            boundedStart, boundedStop = grid.boundaries(start, stop) # ensures that start/stop coordinates are within boundaries of grid
             
+            ''' Depending on the command (turn on, turn off, toggle), the necessary method is called'''
             if cleanFile[i][0] == 'turn on':
                 grid.turn_on(boundedStart, boundedStop)
             elif cleanFile[i][0] == 'turn off':
                 grid.turn_off(boundedStart, boundedStop)
             elif cleanFile[i][0] == 'switch':
                 grid.toggle(boundedStart, boundedStop)
-            
+        
+        ''' Call the count method and print the results'''    
         print("No. of Lights On:", grid.count(gridSize))
 
 if __name__ == '__main__':
